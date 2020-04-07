1dd38b6a8f67ab860fbcda0d7d9b836b377a0ca8
kmcauliffe6 <kmcauliffe6@gatech.edu>
2020-04-07 19:41:59 -0400

    Initial Commit

---
+++ CS4440/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS4440
+//
+//  Created by Paige McAuliffe on 4/7/20.
+//  Copyright © 2020 Georgia Teach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
