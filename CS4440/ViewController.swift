47959da0d8d9d4567f706b01c8a9e3dc6b0533e5
kmcauliffe6 <kmcauliffe6@gatech.edu>
2020-04-07 20:48:36 -0400

    first commmit - setting up basic UI

---
+++ CS4440/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CS4440
+//
+//  Created by Paige McAuliffe on 4/7/20.
+//  Copyright © 2020 Georgia Teach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
