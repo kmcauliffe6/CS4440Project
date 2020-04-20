c6601bb021df52223843b712ca4336e172411f6c
kmcauliffe6 <kmcauliffe6@gatech.edu>
2020-04-20 13:47:43 -0400

    connected to Twitter API with Swifter, set up test query

--- CS4440/ViewController.swift
+++ CS4440/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
-
+import SwifteriOS
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    // Instantiation using Twitter's OAuth Consumer Key and secret
+    let swifter = Swifter(consumerKey: "API KEY", consumerSecret: "API SECRET")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // using Twitter Standard Search API
+        //https://developer.twitter.com/en/docs/tweets/search/overview/standard
+        //return a collection of relevant tweets based on a query
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 50, tweetMode: TweetMode.extended, success: { (results, searchMetadata) in
+            // 'text' field of JSON holds the tweet message
+            print(results)
+        }) { (err) in
+            print("Error occured connecting to Twitter API")
+        }
         
     }
 
