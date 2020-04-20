fd4d85c2155b064281002d212a34c605449bbaa4
kmcauliffe6 <kmcauliffe6@gatech.edu>
2020-04-20 15:42:49 -0400

    set up running sentiment analysis and scoring sentiment based on results

--- CS4440/ViewController.swift
+++ CS4440/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var sentimentLabel: UILabel!
     
     // Instantiation using Twitter's OAuth Consumer Key and secret
-   let swifter = Swifter(consumerKey: "API Key", consumerSecret: "API Secret")
+   let swifter = Swifter(consumerKey: "UhIGvRC8EL7QDo0aXs2Hwjv0B", consumerSecret: "5Qv87L0V2FtLR8tKkj02mf7hwY5mMUkKB8qTCF5oDpHoNJ7KRK")
     
     let classifier = TwitterSentimentClassifer()
         
@@ -31,10 +31,40 @@ class ViewController: UIViewController {
         // using Twitter Standard Search API
         //https://developer.twitter.com/en/docs/tweets/search/overview/standard
         //return a collection of relevant tweets based on a query
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 50, tweetMode: TweetMode.extended, success: { (results, searchMetadata) in
-            // 'full-text' field of JSON holds the tweet message
+        swifter.searchTweet(using: "Facebook", lang: "en", count: 50, tweetMode: TweetMode.extended, success: { (results, searchMetadata) in
+            // 'full_text' field of JSON holds the tweet message
+            var tweets = [TwitterSentimentClassiferInput]()
             
-            print(results)
+            //get 50 tweets about the input text
+            for x in 0..<50 {
+                if let tweet_msg = results[x]["full_text"].string {
+                    tweets.append(.init(text: tweet_msg))
+                }
+            }
+            
+            // do sentiment analysis on the tweets
+            do {
+                let predictions = try self.classifier.predictions(inputs: tweets)
+                
+                // score general sentiment
+                var sScore = 0
+                for p in predictions {
+                    let sent = p.label
+                    if sent == "Pos" {
+                         sScore += 1
+                    }
+                    else if sent == "Neg" {
+                        sScore -= 1
+                    }
+                }
+                print(sScore)
+                
+                
+            } catch {
+                print("Error classifying tweets")
+            }
+            
+            
         }) { (err) in
             print("Error occured connecting to Twitter API")
         }
